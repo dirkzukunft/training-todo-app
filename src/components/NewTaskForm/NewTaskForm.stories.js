3e44d054ef888cd5ae1f8b55140b72049402ea7b
dirkzukunft <git@qf3.de>
2021-07-22 15:48:19 +0200

    Basic setup

---
+++ src/components/NewTaskForm/NewTaskForm.stories.js
@@ -0,0 +1,9 @@
+import "./NewTaskForm.css";
+import compontentNewTaskForm from "./NewTaskForm.html";
+
+export default {
+  title: "Components/NewTaskForm",
+  parameters: { layout: "fullscreen" },
+};
+
+export const NewTaskForm = () => compontentNewTaskForm;
