4e9a5f557b09411354ffce447ffd9193712d0574
dirkzukunft <git@qf3.de>
2021-07-25 13:57:47 +0200

    Add FilterNav HTML and CSS

---
+++ src/components/FilterNav/FilterNav.stories.js
@@ -0,0 +1,9 @@
+import "./FilterNav.css";
+import compontentFilterNav from "./FilterNav.html";
+
+export default {
+  title: "Components/FilterNav",
+  parameters: { layout: "fullscreen" },
+};
+
+export const FilterNav = () => compontentFilterNav;
